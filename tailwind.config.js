a68ef9558c13d1d79c5bd7e123d737c027cc3abb
Quazi Md Aldin Fardin <59772487+Taraldinn@users.noreply.github.com>
2022-06-27 01:15:56 +0600

    done
    but with gui

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}",
+            "./*.{html,js,css}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+
+  ],
+}
